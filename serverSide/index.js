eab7f847985b61f7b8200fb2b2d679432ecb9f1f
ali heydari <heidari50000@gmail.com>
2021-07-18 23:49:30 +0800

    server side

---
+++ serverSide/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+var cors = require('cors')
+const app = express()
+const port = 3000
+
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send({id:1,name:'name',des:'des',some:Math.random()})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
